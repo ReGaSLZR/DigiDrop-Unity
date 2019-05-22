83774cb8ddf0563016e3c58213378f0de143fc9b
RenSalazar <salazarrenelie@gmail.com>
2019-05-23 00:52:19 +0800

    Committing files under "Scripts/Common" folder

---
+++ Scripts/Common/PlayerPrefKey.cs
@@ -0,0 +1,20 @@
+﻿public class PlayerPrefKey {
+
+	public const string TIME = "key_time";
+	public const string COIN_WAVE_INTERVAL_MIN = "key_coinwaveinterval_min";
+	public const string COIN_WAVE_INTERVAL_MAX = "key_coinwaveinterval_max";
+	public const string COINS_PER_WAVE_MIN = "key_coinsperwave_min";
+	public const string COINS_PER_WAVE_MAX = "key_coinsperwave_max";
+
+	public const string DEVICE_INPUT_CONTROL_CHOICE = "key_device_input_control_choice";
+
+	public const string ACCEL_FRAME_WINDOW = "accel_frame_window";
+
+}
+
+public class PlayerPrefValue {
+
+	public const string DEVICE_INPUT_CONTROL_CHOICE_GYRO = "Gyroscope";
+	public const string DEVICE_INPUT_CONTROL_CHOICE_ACCEL = "Accelerometer";
+
+}
---
+++ Scripts/Common/Tag.cs
@@ -0,0 +1,11 @@
+﻿
+/*
+	NOTE: This reflects all the other tags that the game project uses and are declared on Tags.
+	To check, please see Edit > Project Settings > Tags and Layers
+*/
+public class Tag 
+{
+
+	public static string COIN = "coin";
+
+}
